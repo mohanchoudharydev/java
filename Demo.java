0fc09343c8d52823992fe4db3c1dca0abf3828d3
mohanchoudharydev <mohanchoudhary7055@gmail.com>
2023-05-02 12:48:03 +0530

    1

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+    public static void main (String arr[])
+
+    {
+        System.out.println("Name= Mohan Choudhary ");
+        System.out.println("vallageName= narwar ");
+
+    }
+}
