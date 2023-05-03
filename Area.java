20800683941ba0036c4c0e761cdbd98b09b42139
mohanchoudharydev <mohanchoudhary7055@gmail.com>
2023-05-03 11:08:28 +0530

    md

---
+++ Area.java
@@ -0,0 +1,8 @@
+ public class Area{
+    public static void main( String arr[])
+    {
+        int a=10,b=20,area;
+        area = a * b;
+        System.out.print("Area of Ractangle " + area);
+    }
+}
