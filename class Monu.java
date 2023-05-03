c506676f091bc540b414e2cb5c9870219bb281d6
mohanchoudharydev <mohanchoudhary7055@gmail.com>
2023-05-03 11:38:23 +0530

    Input num

---
+++ class Monu.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Monu
+{
+    public static void main(String arr[])
+    {
+        int a;
+        System.out.println("Enter a no");
+        Scanner ob=new Scanner (System.in);
+        a=ob.nextInt();
+        System.out.println("You entered" + a);
+        
+
+    }
+}
